497554fd8dbf0e3c4d98f7c186a7ac21684eaf83
LuddyFish <jackjumper.l03@gmail.com>
2025-07-02 15:56:43 +1000

    Built a mobile version nav-bar

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,13 +1,31 @@
 const nav = document.getElementById("nav-bar");
+const hamburger = document.getElementById('hamburger');
+const sidebar = document.querySelector('.sidebar');
 let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll", () => {
-    // If scrolling down
-    if (lastScrollY < window.scrollY) {
-        nav.classList.add("nav-hidden");
-    } else {
-        nav.classList.remove("nav-hidden");
-    }
+const mobileMaxWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--mobile-max-width'));
+const isMobileMedia = window.matchMedia(`(max-width: ${mobileMaxWidth}px)`);
 
-    lastScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+    if (!isMobileMedia.matches) {
+        // If scrolling down
+        if (lastScrollY < window.scrollY) {
+            nav.classList.add("nav-hidden");
+        } else {
+            nav.classList.remove("nav-hidden");
+        }
+
+        lastScrollY = window.scrollY;
+    }
+});
+
+isMobileMedia.addEventListener('change', () => {
+    nav.classList.remove('nav-hidden');
+    sidebar.classList.remove('active');
+});
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    sidebar.classList.toggle('active');
+    hamburger.innerHTML = hamburger.classList.contains('active') ? "&#10006;" : "&#9776;";
 });
